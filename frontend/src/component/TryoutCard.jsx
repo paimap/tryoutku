0468fe8e08f7e80374d7cd9785cf3e33fa817193
Paima <paimapurba@gmail.com>
2025-03-07 01:51:07 +0700

    add tryout card

--- frontend/src/component/TryoutCard.jsx
+++ frontend/src/component/TryoutCard.jsx
@@ -1,3 +1,19 @@
-function TryoutCard() {
-    
+import DateRangeIcon from '@mui/icons-material/DateRange';
+import CategoryIcon from '@mui/icons-material/Category';
+import "../css/TryoutCard.css"
+import CardInfo from './CardInfo';
+
+function TryoutCard({detail}) {
+    return (
+        <div className="card-grid">
+            <div className="to-card">
+                <h1>{detail.title}</h1>
+                <hr />
+                <CardInfo icon={<DateRangeIcon fontSize="medium" className="date-icon"/>} text={detail.date} />
+                <CardInfo icon={<CategoryIcon fontSize="medium" className="category-icon"/>} text={detail.category} />
+            </div>
+        </div>
+    )
 }
+
+export default TryoutCard
